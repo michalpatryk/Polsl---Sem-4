b6ed363ff9e5d2a5460c5f8c2b7ab2bebbe9a9aa
Michał Urbańczyk <urbanczyka3k@gmail.com>
2020-05-25 20:32:24 +0200

    Gui added

---
+++ Projekt-ReactorGame/Projekt-ReactorGame/debugFunctions.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <Windows.h>
+
+void getFullPath(std::string str)
+{
+	char filename[100];
+	strcpy_s(filename, str.c_str());
+	char fullFilename[MAX_PATH];
+	GetFullPathName(filename, MAX_PATH, fullFilename, nullptr);
+	MessageBox(NULL, fullFilename, "DEBUG", MB_OK);
+	return;
+}
