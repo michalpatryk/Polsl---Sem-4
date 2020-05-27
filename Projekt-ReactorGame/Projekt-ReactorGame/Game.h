6e52d9dccea1e598443c01037cdd2af99bd67e48
Michał Urbańczyk <urbanczyka3k@gmail.com>
2020-05-27 12:24:59 +0200

    tileMap done

--- Projekt-ReactorGame/Projekt-ReactorGame/Game.h
+++ Projekt-ReactorGame/Projekt-ReactorGame/Game.h
@@ -7,11 +7,15 @@ class Game
 {
 	sf::RenderWindow window{};
 	sf::VideoMode vMode{};
+	std::vector<int> level;
+	
 public:
 	explicit Game(const sf::VideoMode VMode) {
 		vMode = VMode;
 	};
 	int run();
 	int TSP(int x, int y) { return(x + y * 23); }
+	std::vector<int> levelGenerator(std::string levelType);
+
 };
 
